16d9e07258437cf906c49435ffb3a30a20b8033b
SuzukiYusuke <knolliven280@gmail.com>
2025-05-03 22:26:22 +0900

    postProcess modified

--- post.sh
+++ post.sh
@@ -1,8 +1,23 @@
 # !/bin/bash
-case_name="cavity_smac_09"
 
-for i in {0..25}
+# cavity
+# start="0"
+# end="250000"
+# interval="10000"
+# case_name="cavity_smac_09"
+
+# couette
+start="0"
+end="2500000"
+interval="100000"
+case_name="couette_smac_04"
+
+rm ${case_name}/energy.dat
+touch ${case_name}/energy.dat
+echo "TimeStep KineticEnergy ViscousDisspition WallWork massBalance" > ${case_name}/energy.dat
+
+# for ( i=start; i<=end; i++ ))
+for time in $(seq ${start} ${interval} ${end}) 
 do
-    time=$(( i*10000 ))
     ./postProcess -c ${case_name} -t ${time} >> ${case_name}/energy.dat
 done
